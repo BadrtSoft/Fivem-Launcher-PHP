904f4cdc04ffd19b1757dd211e405ec827a07b8c
Yalcin Gormez <gitkraken@yalc.in>
2020-06-10 03:41:07 +0300

    Steam api key kolay değişebilmesi için değişken haline getirildi

--- PHP/steamProxy.php
+++ PHP/steamProxy.php
@@ -1,6 +1,7 @@
 <?PHP
 
-$url = 'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=STEAMKEY&steamids=' . $_GET['id'];
+$steamApiKey = 'STEAM API KEY YAZINIZ';
+$url = 'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=' . $steamApiKey . '&steamids=' . $_GET['id'];
  
 //Once again, we use file_get_contents to GET the URL in question.
 $contents = file_get_contents($url);
