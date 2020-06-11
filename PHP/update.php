19df990d4842b42c330f9bd3dbc06ed5fe9d63ee
Yalcin Gormez <gitkraken@yalc.in>
2020-06-11 03:24:03 +0300

    Deneme amaçlı yazılan notepad silindi

--- PHP/update.php
+++ PHP/update.php
@@ -5,5 +5,5 @@
 	"discord": "https://discord.gg/e9URnEe", 
 	"teamspeak3": "", 
 	"server_code": "",
-	cheats: ["2Take1Free","Cheat Engine","cheatengine-x86_64","Dexmenu","Eulen","FX Premium","GH Injector","hammafia","injection","injector","Kaynak \u0130zleyicisi","Lynx","machine therapist","machine_therapist","premium","Process Hacker","Renamenu","RobloxPlayerBeta","root","notepad"]
+	"cheats": ["2Take1Free","Cheat Engine","cheatengine-x86_64","Dexmenu","Eulen","FX Premium","GH Injector","hammafia","injection","injector","Kaynak \u0130zleyicisi","Lynx","machine therapist","machine_therapist","premium","Process Hacker","Renamenu","RobloxPlayerBeta","root"]
 }
