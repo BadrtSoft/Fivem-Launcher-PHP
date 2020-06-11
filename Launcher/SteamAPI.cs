9babcec1008a2520345ef1f1552ad29f649be053
Yalcin Gormez <gitkraken@yalc.in>
2020-06-11 03:19:07 +0300

    Gereksiz satirlar silindi

--- Launcher/SteamAPI.cs
+++ Launcher/SteamAPI.cs
@@ -44,55 +44,16 @@ namespace Launcher
         [JsonProperty("steamid")]
         public string Steamid { get; set; }
 
-        [JsonProperty("communityvisibilitystate")]
-        public long Communityvisibilitystate { get; set; }
-
-        [JsonProperty("profilestate")]
-        public long Profilestate { get; set; }
-
         [JsonProperty("personaname")]
         public string Personaname { get; set; }
 
         [JsonProperty("profileurl")]
-        public Uri Profileurl { get; set; }
+        public string Profileurl { get; set; }
 
         [JsonProperty("avatar")]
-        public Uri Avatar { get; set; }
-
-        [JsonProperty("avatarmedium")]
-        public Uri Avatarmedium { get; set; }
+        public string Avatar { get; set; }
 
         [JsonProperty("avatarfull")]
-        public Uri Avatarfull { get; set; }
-
-        [JsonProperty("avatarhash")]
-        public string Avatarhash { get; set; }
-
-        [JsonProperty("lastlogoff")]
-        public long Lastlogoff { get; set; }
-
-        [JsonProperty("personastate")]
-        public long Personastate { get; set; }
-
-        [JsonProperty("realname")]
-        public string Realname { get; set; }
-
-        [JsonProperty("primaryclanid")]
-        public string Primaryclanid { get; set; }
-
-        [JsonProperty("timecreated")]
-        public long Timecreated { get; set; }
-
-        [JsonProperty("personastateflags")]
-        public long Personastateflags { get; set; }
-
-        [JsonProperty("loccountrycode")]
-        public string Loccountrycode { get; set; }
-
-        [JsonProperty("locstatecode")]
-        public long Locstatecode { get; set; }
-
-        [JsonProperty("loccityid")]
-        public long Loccityid { get; set; }
+        public string Avatarfull { get; set; }
     }
 }
